404338cc8aacccadaf5d5d08d6957773285b7dd4
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-02-25 20:11:54 -0300

    req 2 done

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,12 +1,17 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path) -> List[Dict]:
+        with open(path, encoding="utf-8") as file:
+            dict_csv = csv.DictReader(file, delimiter=",", quotechar='"')
+            for row in dict_csv:
+                self.jobs_list.append(row)
+        return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
         pass
