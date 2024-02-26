79b5c7c005157ec4882cbb1b0f72c0829cec176e
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-02-26 09:56:03 -0300

    req 6 and 7 done

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -7,10 +7,22 @@ class ProcessSalaries(ProcessJobs):
         super().__init__()
 
     def get_max_salary(self) -> int:
-        pass
+        list_new = []
+        for row in self.jobs_list:
+            if len(row["max_salary"]) != 0 and row["max_salary"].isdigit():
+                salary = int(row["max_salary"])
+                list_new.append(salary)
+        max_salary = max(list_new)
+        return max_salary
 
     def get_min_salary(self) -> int:
-        pass
+        list_new = []
+        for row in self.jobs_list:
+            if len(row["min_salary"]) != 0 and row["min_salary"].isdigit():
+                salary = int(row["min_salary"])
+                list_new.append(salary)
+        min_salary = min(list_new)
+        return min_salary
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
         pass
