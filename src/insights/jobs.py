81ccdc23c2642e59336989774c9f37bf5a671646
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-02-26 01:21:15 -0300

    req 4 done

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,6 +1,11 @@
 from typing import List, Dict
 import csv
 
+jobs = [
+    {"id": 1, "industry": "IT", "job_type": "FULL_TIME"},
+    {"id": 2, "industry": "Healthcare", "job_type": "PART_TIME"},
+]
+
 
 class ProcessJobs:
     def __init__(self) -> None:
@@ -21,5 +26,16 @@ class ProcessJobs:
 
         return list_type_jobs
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def filter_by_multiple_criteria(
+        self, list_dict_jobs, filter_criteria
+    ) -> List[dict]:
+        filters_jobs = []
+        if type(filter_criteria) is not dict:
+            raise TypeError
+        for job in list_dict_jobs:
+            if (
+                job["industry"] == filter_criteria["industry"]
+                and job["job_type"] == filter_criteria["job_type"]
+            ):
+                filters_jobs.append(job)
+        return filters_jobs
